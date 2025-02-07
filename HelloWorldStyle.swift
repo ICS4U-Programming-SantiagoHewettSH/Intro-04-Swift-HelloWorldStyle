c4aa589a2a13b43a444c73353acb147cec729e85
Santiago Hewett <santiago.hewett@stu.ocsb.ca>
2025-02-07 11:55:34 +0000

    Hello World Style Swift Done

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,21 @@
+//  HelloWorld.swift
+
+//
+
+//  Created by Santiago Hewett
+
+//  Created on 2025/02/06
+
+//  Version 1.0
+
+//  Copyright (c) 2025 . All rights reserved.
+
+//
+
+//  This program greets my friend and asks them how they are.
+
+import Foundation  // Imports the Foundation module
+
+// Prints the message to the console
+print("Hello, Remy. ")
+print ("How are you?")
